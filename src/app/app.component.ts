64e3c6561fd679313254a1d7e6c1e6228e4190c3
Ward Vanlerberghe <ward.vanlerberghe.i2568@student.hogent.be>
2017-11-19 19:31:39 +0100

    Changed app.component.ts to show db users info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Define a users property to hold our user data
+  users: Array<any>;
+  
+    // Create an instance of the DataService through dependency injection
+    constructor(private _dataService: DataService) {
+  
+      // Access the Data Service's getUsers() method we defined
+      this._dataService.getUsers()
+          .subscribe(res => this.users = res);
 }
